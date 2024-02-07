045110c53fc250244261a5b1e88db795258cf646
Roman Bolkhovitin <bolkhovitin_rv@itms.tech>
2024-02-07 19:23:12 +0300

    page scrolling and lsp settings

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -26,3 +26,7 @@ vim.keymap.set({ "v", "n" }, "<up>", "<nop>", { desc = "disable arrows" })
 vim.keymap.set({ "v", "n" }, "<down>", "<nop>", { desc = "disable arrows" })
 vim.keymap.set({ "v", "n" }, "<left>", "<nop>", { desc = "disable arrows" })
 vim.keymap.set({ "v", "n" }, "<right>", "<nop>", { desc = "disable arrows" })
+
+-- half page scrolling
+vim.keymap.set("n", "<c-d>", "<c-d>zz", { desc = "scroll half page and center" })
+vim.keymap.set("n", "<c-u>", "<c-u>zz", { desc = "scroll half page and center" })
--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,10 +1,31 @@
 return {
   "neovim/nvim-lspconfig",
+  ---@class PluginLspOpts
   opts = {
     capabilities = {
       workspace = {
         didChangeWatchedFiles = {
           dynamicRegistration = true,
+        },
+      },
+    },
+    ---@type lspconfig.options
+    servers = {
+      gopls = {
+        settings = {
+          gopls = {
+            analyses = {
+              -- defaults
+              -- fieldalignment = true,
+              -- nilness = true,
+              -- unusedparams = true,
+              -- unusedwrite = true,
+              -- useany = true,
+              -- custom
+              unusedvariable = true,
+              shadow = true,
+            },
+          },
         },
       },
     },
