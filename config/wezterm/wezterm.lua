4f51b19ffe51a3f7ba1286be1c7b1171110d58ed
Roman Bolkhovitin <bolkhovitin_rv@itms.tech>
2024-02-24 00:33:25 +0300

    disable ligatures

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -6,7 +6,7 @@ local os = require("os")
 local config = {}
 
 config.hide_tab_bar_if_only_one_tab = true
-config.font = wezterm.font("FiraCode Nerd Font")
+config.font = wezterm.font({ family = "FiraCode Nerd Font", harfbuzz_features = { "calt=0", "clig=0", "liga=0" } })
 config.font_size = 14
 config.default_prog = { "tmux" }
 config.audible_bell = "Disabled"
