7ae1de47da43467cac28f27bf5c2b3dddfe7565f
roman <roman@localhost.localdomain>
2024-02-25 13:21:56 +0300

    fix after check on linux

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -2,12 +2,26 @@
 local wezterm = require("wezterm")
 local os = require("os")
 
+-- helpers
+local is_linux = function()
+	return wezterm.target_triple:find("linux") ~= nil
+end
+
+local is_darwin = function()
+	return wezterm.target_triple:find("darwin") ~= nil
+end
+
 -- This table will hold the configuration.
-local config = {}
+local config = wezterm.config_builder()
 
 config.hide_tab_bar_if_only_one_tab = true
 config.font = wezterm.font({ family = "FiraCode Nerd Font", harfbuzz_features = { "calt=0", "clig=0", "liga=0" } })
+
 config.font_size = 14
+if is_linux() then
+	config.font_size = 12
+end
+
 config.default_prog = { "tmux" }
 config.audible_bell = "Disabled"
 
